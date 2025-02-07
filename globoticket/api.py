0cd5ce83dfcb8337cc8be01e0d8f9d48a85fc4ac
benmuratseker <benmuratseker@hotmail.com>
2025-02-07 23:35:54 +0100

    get all events operation added

--- globoticket/api.py
+++ globoticket/api.py
@@ -5,6 +5,7 @@ The REST Api for the events database.
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from starlette.staticfiles import StaticFiles
 from pathlib import Path
@@ -26,13 +27,21 @@ def get_session() -> Session:
         session.close()
 
 # @app.get("/event/{id}")
-@app.get("/event/{id}", response_model=Event)
+@app.get(path="/event/{id}", response_model=Event)
 def get_event(id: int, db: Annotated[Session, Depends(get_session)]) -> DBEvent:
     """Retrieve a single event by id. Returns status 404 if event is not found."""
     event = db.get(DBEvent, id)
     if event is None:
         raise HTTPException(status_code=404, detail=f"No event with id {id}")
     return event
+
+@app.get(path="/event/", response_model=list[Event])
+def get_all_events(db: Annotated[Session, Depends(get_session)]) -> list[DBEvent]:
+    """Retrieves all events."""
+    events = db.execute(select(DBEvent)).scalars()
+    if events is None:
+        raise HTTPException(status_code=404, detail=f"There is no event yet")
+    return events
 
 @app.get("/hello")
 def hello() -> str:
